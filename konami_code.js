5e1eb26ef2f65c33fa4c756ec62f536572ad5de2
harenarium <22226614+harenarium@users.noreply.github.com>
2018-03-17 20:29:02 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,33 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(){
 
+  let index = 0;
+
+  document.body.addEventListener('keydown',
+
+    (event) => {
+      // Write your JavaScript code inside the init() function
+      console.log(event)
+      const key = parseInt(event.detail || event.which || event.location);
+
+      if (key === code[index]) {
+        index++;
+        console.log(key)
+        console.log(index)
+        if (index === code.length) {
+          alert("Hurray!");
+          index = 0;
+        }
+      } else if (key === 38){
+        index = 1;
+        console.log(key)
+        console.log(index)
+      } else {
+        index = 0;
+        console.log(key)
+        console.log(index)
+      }
+    }
+  )
 }
